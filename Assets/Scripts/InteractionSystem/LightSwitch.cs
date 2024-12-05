97d5e9d9d61f00124bd1acd4933f4f755c6e92ed
Carlos Emilio Guidi Cordero <emiliogc06@gmail.com>
2024-12-05 01:26:18 -0300

    switches

--- Assets/Scripts/InteractionSystem/LightSwitch.cs
+++ Assets/Scripts/InteractionSystem/LightSwitch.cs
@@ -5,9 +5,42 @@ using UnityEngine;
 public class LightSwitch : MonoBehaviour
 {
     [SerializeField] private List<Light> lights;
+    [SerializeField] private bool isOn;
+    [SerializeField] private List<GameObject> lightSwitches;
+
+    private void Awake()
+    {
+        changeState();
+    }
 
     public void changeState()
     {
-        foreach (Light light in lights) { light.enabled = !light.enabled; }
+        switchRotation();
+        OnOffState();
+        isOn = !isOn;
+    }
+
+    private void switchRotation()
+    {
+        if (isOn)
+        {
+            foreach (GameObject lightSwitch in lightSwitches) { lightSwitch.transform.localEulerAngles = new Vector3(0, 0, 0); }
+        }
+        else
+        {
+            foreach (GameObject lightSwitch in lightSwitches) { lightSwitch.transform.localEulerAngles = new Vector3(60, 0, 0); }
+        }
+    }
+
+    private void OnOffState()
+    {
+        if (isOn)
+        {
+            foreach (Light light in lights) { light.enabled = true; }
+        }
+        else
+        {
+            foreach (Light light in lights) { light.enabled = false; }
+        }
     }
 }
