8627bc5e94bb6f21dd379c4b48babd6884d2f549
Carlos Emilio Guidi Cordero <emiliogc06@gmail.com>
2024-12-05 13:05:46 -0300

    changes audio

--- Assets/Scripts/Sound/AudioManager.cs
+++ Assets/Scripts/Sound/AudioManager.cs
@@ -8,29 +8,73 @@ public class AudioManager : MonoBehaviour
     public static AudioManager Instance;
 
     public Sound[] musicSounds, sfxSounds;
-    public AudioSource musicSource, sfxSource;
+    public AudioSource musicSource;
+    public List<AudioSource> sfxSources;
+
+    
+
+    private void Awake()
+    {
+        if (Instance == null) 
+        {
+            Instance = this; DontDestroyOnLoad(gameObject); 
+        }
+        else 
+        {
+            Destroy(gameObject); 
+        }
+    }
 
     private void Start()
     {
         PlayMusic("house1");
     }
 
-    private void Awake()
-    {
-        if (Instance == null) { Instance = this; DontDestroyOnLoad(gameObject); }
-        else { Destroy(gameObject); }
-    }
-
     public void PlayMusic(string name)
     {
         Sound sound = Array.Find(musicSounds, x => x.soundName == name);
-        if (sound == null) { Debug.Log("Sound " + name + " not found"); }
-        else{ musicSource.clip = sound.clip; musicSource.Play(); }
+        
+        if (musicSource != null && sound != null)
+        {
+            musicSource.clip = sound.clip;
+            musicSource.loop = true;
+            musicSource.spatialBlend = 0f;
+            musicSource.Play(); 
+        }
+        else
+        {
+            Debug.Log("Sound " + name + " not found");
+        }
     }
     public void PlaySFX(string name)
     {
         Sound sound = Array.Find(sfxSounds, x => x.soundName == name);
-        if (sound == null) { Debug.Log("sfx " + name + " not found"); }
-        else{ sfxSource.clip = sound.clip; sfxSource.Play(); }
+
+        //if (sound == null) 
+        //{ 
+        //    Debug.Log("sfx " + name + " not found"); 
+        //}
+        //else
+        //{ 
+        //    sfxSource.clip = sound.clip; sfxSource.Play(); 
+        //}
+        if(sound == null) 
+        {
+            Debug.Log("sfx " + name + " not found"); 
+        }
+        else
+        {
+            foreach (AudioSource source in sfxSources)
+            {
+                if (!source.isPlaying)
+                {
+                    source.clip = sound.clip;
+                    source.spatialBlend = 1f;
+                    source.Play();
+                    return;
+                }
+            }
+        }
+        
     }
 }
